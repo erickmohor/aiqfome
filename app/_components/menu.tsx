0cc127cba2352aa7e07e6638d745d9227c18cfef
erick-1680144722 <erick.mohor@gmail.com>
2025-05-01 22:54:51 -0300

    refactor: change menu export

--- app/_components/menu.tsx
+++ app/_components/menu.tsx
@@ -1,6 +1,6 @@
-import ProductListByCategory from "./product-list-by-category";
+import { ProductListByCategory } from "./product-list-by-category";
 
-export default function Menu() {
+export function Menu() {
   return (
     <div className="bg-neutrals-100 space-y-1">
       <ProductListByCategory initOpen />
