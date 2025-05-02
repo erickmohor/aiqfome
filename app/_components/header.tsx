bd7e8e5cc31b66214866b4dbfec276572fec1d1b
erick-1680144722 <erick.mohor@gmail.com>
2025-05-01 22:44:24 -0300

    style: adjust the header width size when the screen is larger than sm

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,31 +3,33 @@ import { ChevronRight, MapPin } from "lucide-react";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between gap-6 bg-purple-500 p-4">
-      <Image
-        alt="Aiqfome logo"
-        src="/aiqfome-logo.png"
-        width={32}
-        height={32}
-      />
+    <div className="flex items-center justify-center bg-purple-500">
+      <div className="flex max-w-[700px] flex-1 items-center justify-between gap-6 p-4 sm:w-[700px]">
+        <Image
+          alt="Aiqfome logo"
+          src="/aiqfome-logo.png"
+          width={32}
+          height={32}
+        />
 
-      <div className="flex flex-1 items-center gap-3.5 text-white">
-        <MapPin size={24} />
-        <div>
-          <p className="text-sm font-bold">entregando em</p>
-          <div className="flex items-center gap-1">
-            <p className="text-base font-bold">Rua Mandaguari, 198</p>
-            <ChevronRight size={16} />
+        <div className="flex flex-1 items-center gap-3.5 text-white">
+          <MapPin size={24} />
+          <div>
+            <p className="text-sm font-bold">entregando em</p>
+            <div className="flex items-center gap-1">
+              <p className="text-base font-bold">Rua Mandaguari, 198</p>
+              <ChevronRight size={16} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <Image
-        alt="Ícone de usuário"
-        src="/icons/user.svg"
-        width={16}
-        height={18}
-      />
+        <Image
+          alt="Ícone de usuário"
+          src="/icons/user.svg"
+          width={16}
+          height={18}
+        />
+      </div>
     </div>
   );
 }
