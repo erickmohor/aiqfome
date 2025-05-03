1149ec433cbbe7738558b20291cc0671901e22d1
erick-1680144722 <erick.mohor@gmail.com>
2025-05-03 13:21:33 -0300

    refactor: ensure that clicking on the logo takes the user to the main page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import { ChevronRight, MapPin } from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="flex items-center justify-center bg-purple-500">
       <div className="flex max-w-[700px] flex-1 items-center justify-between gap-6 p-4 sm:w-[700px]">
-        <Image
-          alt="Aiqfome logo"
-          src="/aiqfome-logo.png"
-          width={32}
-          height={32}
-        />
+        <Link href="/">
+          <Image
+            alt="Aiqfome logo"
+            src="/aiqfome-logo.png"
+            width={32}
+            height={32}
+          />
+        </Link>
 
         <div className="flex flex-1 items-center gap-3.5 text-white">
           <MapPin size={24} />
